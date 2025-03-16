c845f255a0518f479acbfe8da2f728639a6602ab
gdemko6 <gregory.demko@my.uwrf.edu>
2025-03-15 20:00:20 -0500

    Add ability for user to upload files using multer

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,9 +1,11 @@
+
+
 function getUpload(req, res) {
     res.render("upload");
 }
 
 function uploadFile(req, res) {
-    
+    console.log(req.file, req.body)
 }
 
 module.exports = { getUpload, uploadFile };
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { uploadFile, getUpload } = require("../controllers/uploadController");
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' });
 
 router.get("/", getUpload);
-router.post("/", uploadFile);
+router.post("/", upload.single('file-upload'), uploadFile);
 
 module.exports = router;
