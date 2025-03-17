0d9a51b618dd4ebde64d709b9468888b13639477
gdemko6 <gregory.demko@my.uwrf.edu>
2025-03-16 20:36:35 -0500

    Fix typo 'users' to 'user'

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -10,10 +10,10 @@ async function signUpUser(req, res) {
     const user = req.body;
     const password = await hashPassword(user.password);
     try {
-        await prisma.users.create({
+        await prisma.user.create({
             data: { email: user.email, password: password }
         })
-        res.send("you have succesfully signed up.");
+        res.redirect("/login");
     } catch (error) {
         console.error(error);
         res.status(500).send("Unsuccessful sign-up.");
