4a23c44dfad050be4ce30d1015744faf6e8e0a74
gdemko6 <gregory.demko@my.uwrf.edu>
2025-04-04 18:32:08 -0500

    Change app to reflect the new API

--- app.js
+++ app.js
@@ -4,41 +4,45 @@ require("dotenv").config();
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const passport = require("./auth");
 const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
+
 const app = express();
 const prisma = new PrismaClient();
-const signupRouter = require("./routes/signupRoutes.js");
-const loginRouter = require("./routes/loginRoutes.js");
-const uploadRouter = require("./routes/uploadRoutes.js");
-const folderRouter = require("./routes/folderRoutes.js");
 
-app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: true}));
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 1000 * 60 * 60 * 24 }, 
-      store: new PrismaSessionStore(prisma, {
-        checkPeriod: 500 * 60 * 1000, 
-      }),
-    })
-  );
-  
-  app.use(passport.initialize());
-  app.use(passport.session());
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 500 * 60 * 1000,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 app.use("/upload", uploadRouter);
 app.use("/folders", folderRouter);
 
+
 app.get("/", (req, res) => {
-  res.render("home");
-})
+  res.json({ message: "API is running" });
+});
+
 
 app.listen(3000, () => {
-    console.log("listening on port 3000");
-})
+  console.log("Listening on port 3000");
+});
