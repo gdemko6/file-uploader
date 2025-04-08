486db42512bfe8ce8b1bc05aba24efb94022ace0
gdemko6 <gregory.demko@my.uwrf.edu>
2025-04-08 18:26:31 -0500

    Create a page to display all user's folders

--- frontend/src/pages/FoldersPage.jsx
+++ frontend/src/pages/FoldersPage.jsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Folder from "../components/Folder";
+
 export default function FoldersPage() {
-  return <div className="p-6 text-xl">All Folders</div>;
+  const [folders, setFolders] = useState([]);
+
+  useEffect(() => {
+    const fetchFolders = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/folders", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+
+        const data = await res.json();
+        setFolders(data.folders);
+      } catch (err) {
+        console.error("Error fetching folders:", err.message);
+      }
+    };
+    fetchFolders();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-2 justify-between w-full gap-10 min-h-200 px-6 pt-15 pb-30">
+      {folders &&
+        folders.map((folder) => <Folder key={folder.id} folder={folder} />)}
+    </div>
+  );
 }
