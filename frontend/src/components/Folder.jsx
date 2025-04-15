381edcddeba2ffe019ed8d0eff4d0efb921df34d
gdemko6 <gregory.demko@my.uwrf.edu>
2025-04-15 18:37:15 -0500

    Make folder component have a min height

--- frontend/src/components/Folder.jsx
+++ frontend/src/components/Folder.jsx
@@ -2,7 +2,7 @@ import { FaFolder } from "react-icons/fa";
 
 export default function Folder({ folder }) {
   return (
-    <div className="rounded-md border border-gray-200 border-solid-3 shadow-sm flex flex-col pl-4 justify-center hover:shadow-md transition transform hover:scale-105 hover:cursor-pointer">
+    <div className="min-h-16 rounded-md border border-gray-200 border-solid-3 shadow-sm flex flex-col pl-4 justify-center hover:shadow-md transition transform hover:scale-105 hover:cursor-pointer">
       <div className="flex items-center gap-4 text-blue-500 text-2xl">
         <FaFolder />
         <span className="text-gray-800 text-lg font-medium">{folder.name}</span>
