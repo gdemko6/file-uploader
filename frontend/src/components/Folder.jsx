175f2e39260a3cc02345418b3595bc014359bb7c
Greg Demko <gregory.demko@my.uwrf.edu>
2025-04-21 19:08:55 -0500

    Add a delete button with a trash icon. Need to fix error

--- frontend/src/components/Folder.jsx
+++ frontend/src/components/Folder.jsx
@@ -1,11 +1,29 @@
 import { FaFolder } from "react-icons/fa";
+import { Trash2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-export default function Folder({ folder }) {
+export default function Folder({ folder, deleteFolder }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/folders/${folder.id}`);
+  };
+
   return (
-    <div className="min-h-16 rounded-md border border-gray-200 border-solid-3 shadow-sm flex flex-col pl-4 justify-center hover:shadow-md transition transform hover:scale-105 hover:cursor-pointer">
+    <div
+      onClick={handleClick}
+      className="min-h-16 rounded-md border border-gray-200 shadow-sm flex flex-col pl-4 justify-center hover:shadow-md transition transform hover:scale-105 hover:cursor-pointer"
+    >
       <div className="flex items-center gap-4 text-blue-500 text-2xl">
         <FaFolder />
         <span className="text-gray-800 text-lg font-medium">{folder.name}</span>
+        <Trash2
+          className="w-5 h-5 mr-5 ml-auto text-red-500 hover:text-red-700"
+          onClick={(e) => {
+            e.stopPropagation();
+            deleteFolder(folder.id);
+          }}
+        />
       </div>
     </div>
   );
