58cc3ce1118d68406760b2aa5a0286cb82cfc230
Greg Demko <gregory.demko@my.uwrf.edu>
2025-05-13 13:13:12 -0500

    Change method of getting file metadata to allow for file cards to be used in any context

--- frontend/src/components/FileCard.jsx
+++ frontend/src/components/FileCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function FileCard({ file, folderId, onDelete }) {
+export default function FileCard({ file, onDelete }) {
   return (
     <li className="flex justify-between items-center border border-blue-200 py-1 px-1.5 rounded shadow-sm bg-white">
       <span className="text-gray-800 font-bold italic mr-5 pl-1">
@@ -9,7 +9,7 @@ export default function FileCard({ file, folderId, onDelete }) {
       <div className="space-x-4">
         {/* Download file */}
         <a
-          href={`http://localhost:3000/folders/${folderId}/${file.id}/download`}
+          href={`http://localhost:3000/files/${file.id}/download`}
           className="text-blue-500 hover:underline"
         >
           Download
